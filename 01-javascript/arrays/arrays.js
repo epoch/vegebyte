67ee498eb2610a46f17ed4fa233cfb5886ec6485
Daniel Tsui <tsui.daniel@gmail.com>
2015-12-09 10:39:20 +1100

    hip hip

---
+++ 01-javascript/arrays/arrays.js
@@ -0,0 +1,13 @@
+console.log('hip hip');
+
+var myArr = ['hip','hip'];
+
+var instruments = ['cello','ukelele','cajon', 'guitar', 'piano', 'drum kit']; 
+
+var colours = ['tomato','plum','linen'];
+
+for (var i=0; i<colours.length; i++) {
+  console.log('my number ' + (i+1) + ' choice is ' + colours[i]);
+}
+
+var positions = ['1st','2nd','3rd','4th','5th','6th'];
